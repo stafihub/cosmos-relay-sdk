5412d6ae97b2e22b87a0bcf6d87df60f9f23c759
tpkeeper <tpkeeper.me@gmail.com>
2022-02-18 22:44:21 +0800

    impl recover

--- chain/event.go
+++ chain/event.go
@@ -1,18 +1,13 @@
 package chain
 
 import (
-	"encoding/hex"
-	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/cosmos/cosmos-sdk/types"
 	xBankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	hubClient "github.com/stafihub/cosmos-relay-sdk/client"
 	"github.com/stafihub/rtoken-relay-core/common/core"
-)
-
-var (
-	ErrEventAttributeNumberUnMatch = errors.New("ErrEventAttributeNumberUnMatch")
 )
 
 func (l *Listener) processBlockEvents(currentBlock int64) error {
@@ -29,42 +24,46 @@ func (l *Listener) processBlockEvents(currentBlock int64) error {
 		return err
 	}
 	for _, tx := range txs {
-		for _, log := range tx.Logs {
-			for _, event := range log.Events {
-				err := l.processStringEvents(poolClient, tx.Tx.GetValue(), tx.Height, tx.TxHash, event)
-				if err != nil {
-					if err == ErrEventAttributeNumberUnMatch {
-						// l.log.Warn("got multisend transfer event", "txHash", tx.TxHash, "event", event)
-						continue
-					}
-					return err
-				}
-			}
+		err := l.processTx(poolClient, tx, false, "", "")
+		if err != nil {
+			return err
 		}
 	}
-
 	return nil
 }
 
-func (l *Listener) processStringEvents(client *hubClient.Client, txValue []byte, height int64, txHash string, event types.StringEvent) error {
+// process tx or recovered tx
+func (l *Listener) processTx(poolClient *hubClient.Client, tx *types.TxResponse, isRecover bool, bonder, signer string) error {
+	if tx.Code != 0 || tx.Empty() {
+		return nil
+	}
+	for _, log := range tx.Logs {
+		for _, event := range log.Events {
+			err := l.processStringEvents(poolClient, tx.Tx.GetValue(), tx.Height, tx.TxHash, event, isRecover, bonder, signer)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (l *Listener) processStringEvents(client *hubClient.Client, txValue []byte, height int64, txHash string, event types.StringEvent, isRecover bool, bonder, signer string) error {
 	switch {
 	case event.Type == xBankTypes.EventTypeTransfer:
 		// not support multisend now
 		if len(event.Attributes) != 3 {
-			return ErrEventAttributeNumberUnMatch
+			l.log.Debug("got multisend transfer event", "txHash", txHash, "event", event)
+			return nil
 		}
 		recipient := event.Attributes[0].Value
+		// not to this pool
 		if !l.hasPool(recipient) {
 			return nil
 		}
 		from := event.Attributes[1].Value
 		amountStr := event.Attributes[2].Value
 
-		m := core.Message{
-			Source:      l.symbol,
-			Destination: core.HubRFIS,
-			Reason:      core.ReasonExeLiquidityBond,
-		}
 		coin, err := types.ParseCoinNormalized(amountStr)
 		if err != nil {
 			return fmt.Errorf("amount format err, %s", err)
@@ -72,12 +71,6 @@ func (l *Listener) processStringEvents(client *hubClient.Client, txValue []byte,
 		if coin.GetDenom() != client.GetDenom() {
 			return fmt.Errorf("transfer denom not equal,expect %s got %s", client.GetDenom(), coin.GetDenom())
 		}
-
-		block, err := client.QueryBlock(height)
-		if err != nil {
-			return err
-		}
-		blockHash := hex.EncodeToString(block.BlockID.Hash)
 
 		done := core.UseSdkConfigContext(hubClient.AccountPrefix)
 		var memoInTx string
@@ -91,16 +84,60 @@ func (l *Listener) processStringEvents(client *hubClient.Client, txValue []byte,
 			done()
 			return fmt.Errorf("tx is not type TxWithMemo, txhash: %s", txHash)
 		}
-		memoInTx = memoTx.GetMemo()
-		if len(memoInTx) == 0 {
-			done()
-			l.log.Warn("received token but no memo", "pool", recipient, "from", from, "txHash", txHash, "coin", amountStr)
-			return nil
-		}
 		done()
 
-		done = core.UseSdkConfigContext("fis")
-		bonder, err := types.AccAddressFromBech32(memoInTx)
+		memoInTx = memoTx.GetMemo()
+
+		if isRecover {
+			//check signer of recover tx == from of this tx
+			if from != signer {
+				l.log.Warn("received token with recover memo, but from!=signer", "pool", recipient, "from", from, "txHash", txHash, "coin", coin.String(), "signer", signer)
+				return nil
+			}
+			proposalExeLiquidityBond := core.ProposalExeLiquidityBond{
+				Denom:  string(l.symbol),
+				Bonder: bonder,
+				Pool:   recipient,
+				Txhash: txHash,
+				Amount: coin.Amount,
+			}
+			m := core.Message{
+				Source:      l.symbol,
+				Destination: core.HubRFIS,
+				Reason:      core.ReasonExeLiquidityBond,
+			}
+			m.Content = proposalExeLiquidityBond
+			l.log.Info("find liquiditybond recover transfer", "msg", m)
+			return l.submitMessage(&m)
+		} else {
+			return l.dealMemo(client, memoInTx, recipient, from, txHash, coin)
+		}
+
+	default:
+		return nil
+	}
+}
+
+// memo case: empty, just return
+// memo case: 1:[address], submit exeLiquidityBond proposal to stafihub
+// memo case: 2:[address]:[txHash], recover for txHash (which is send from signer ?)
+// memo case: unkonw format, just return
+func (l Listener) dealMemo(poolClient *hubClient.Client, memo, recipient, from, txHash string, coin types.Coin) error {
+	if len(memo) == 0 {
+		l.log.Warn("received token but no memo", "pool", recipient, "from", from, "txHash", txHash, "coin", coin.String())
+		return nil
+	}
+
+	split := strings.Split(memo, ":")
+	if len(split) < 2 {
+		l.log.Warn("received token with memo, but unknow format", "pool", recipient, "from", from, "txHash", txHash, "coin", coin.String(), "memo", memo)
+		return nil
+	}
+
+	switch split[0] {
+	case "1":
+		done := core.UseSdkConfigContext("stafi")
+		bonder, err := types.AccAddressFromBech32(split[1])
 		if err != nil {
 			done()
 			return err
@@ -109,17 +146,47 @@ func (l *Listener) processStringEvents(client *hubClient.Client, txValue []byte,
 		done()
 
 		proposalExeLiquidityBond := core.ProposalExeLiquidityBond{
-			Denom:     string(l.symbol),
-			Bonder:    bonderStr,
-			Pool:      recipient,
-			Blockhash: blockHash,
-			Txhash:    txHash,
-			Amount:    coin.Amount,
+			Denom:  string(l.symbol),
+			Bonder: bonderStr,
+			Pool:   recipient,
+			Txhash: txHash,
+			Amount: coin.Amount,
+		}
+		m := core.Message{
+			Source:      l.symbol,
+			Destination: core.HubRFIS,
+			Reason:      core.ReasonExeLiquidityBond,
 		}
 		m.Content = proposalExeLiquidityBond
 		l.log.Info("find liquiditybond transfer", "msg", m)
 		return l.submitMessage(&m)
+	case "2":
+		if len(split) != 3 {
+			l.log.Warn("received token with recover memo, but unknow format", "pool", recipient, "from", from, "txHash", txHash, "coin", coin.String(), "memo", memo)
+			return nil
+		}
+		done := core.UseSdkConfigContext("stafi")
+		bonder, err := types.AccAddressFromBech32(split[1])
+		if err != nil {
+			done()
+			return err
+		}
+		bonderStr := bonder.String()
+		done()
+
+		recoverTxHash := split[2]
+		txRes, err := poolClient.QueryTxByHash(recoverTxHash)
+		if err != nil || txRes.Code != 0 || txRes.Empty() {
+			l.log.Warn("received token with recover memo, but QueryTxByHash failed", "pool", recipient, "from", from, "txHash", txHash, "coin", coin.String(), "memo", memo, "err", err)
+			return nil
+		}
+
+		l.log.Info("received token with recover memo, will deal this txHash", "pool", recipient, "from", from, "txHash", txHash, "recoverTxhash", recoverTxHash, "memo", memo)
+		return l.processTx(poolClient, txRes, true, bonderStr, from)
+
 	default:
+		l.log.Warn("received token with memo, but unknow format", "pool", recipient, "from", from, "txHash", txHash, "coin", coin.String(), "memo", memo)
 		return nil
 	}
+
 }
