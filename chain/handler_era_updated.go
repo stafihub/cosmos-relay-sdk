a8c86e4f262f9d9b067cce49fe16d928f08686b9
tpkeeper <tpkeeper.me@gmail.com>
2025-01-09 22:26:55 +0800

    increase gas

--- chain/handler_era_updated.go
+++ chain/handler_era_updated.go
@@ -110,7 +110,7 @@ func (h *Handler) handleEraPoolUpdatedEvent(m *core.Message) error {
 		Type:       stafiHubXLedgerTypes.TxTypeDealEraUpdated}
 
 	var txHash, txBts []byte
-	for i := 25; i < 30; i++ {
+	for i := 35; i < 40; i++ {
 		//use current seq
 		seq, err := poolClient.GetSequence(0, poolAddress)
 		if err != nil {
